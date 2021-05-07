0c9d4837cf2f9ddf9c9106d5fddb682fe959691a
headacheboy <894808578@qq.com>
2021-05-07 14:50:39 +0800

    Update bert_subword_average.py

--- bert_subword_average.py
+++ bert_subword_average.py
@@ -2,7 +2,7 @@ import torch
 
 # this function average the subword representation when these subwords consist of a word. e.g. vector[victim] = vector[vic##] + vector[tim]
 
-def average_pooling(encoded_layers, token_subword_index):
+def average_pooling(encoded_layers, token_subword_index, pad_token_id):
     batch_size, num_tokens, num_subwords = token_subword_index.size()
     batch_index = torch.arange(batch_size).view(-1, 1, 1).type_as(token_subword_index)
     token_index = torch.arange(num_tokens).view(1, -1, 1).type_as(token_subword_index)
@@ -11,14 +11,14 @@ def average_pooling(encoded_layers, token_subword_index):
         batch_size, num_tokens, num_total_subwords, hidden_size)
     # [batch_size, num_tokens, num_subwords, hidden_size]
     token_reprs = expanded_encoded_layers[batch_index, token_index, token_subword_index]
-    subword_pad_mask = token_subword_index.eq(0).unsqueeze(3).expand(
+    subword_pad_mask = token_subword_index.eq(pad_token_id).unsqueeze(3).expand(
         batch_size, num_tokens, num_subwords, hidden_size)
     token_reprs.masked_fill_(subword_pad_mask, 0)
     # [batch_size, num_tokens, hidden_size]
     sum_token_reprs = torch.sum(token_reprs, dim=2)
     # [batch_size, num_tokens]
-    num_valid_subwords = token_subword_index.ne(0).sum(dim=2)
-    pad_mask = num_valid_subwords.eq(0).long()
+    num_valid_subwords = token_subword_index.ne(pad_token_id).sum(dim=2)
+    pad_mask = num_valid_subwords.eq(pad_token_id).long()
     # Add ones to arrays where there is no valid subword.
     divisor = (num_valid_subwords + pad_mask).unsqueeze(2).type_as(sum_token_reprs)
     # [batch_size, num_tokens, hidden_size]
