e0661e9dd9c3129bfe3144a6407f1be87ff7f701
headacheboy <894808578@qq.com>
2021-05-07 14:49:06 +0800

    Update bert_subword_average.py

--- bert_subword_average.py
+++ bert_subword_average.py
@@ -29,6 +29,7 @@ if __name__ == '__main__':
     token_subword_index = torch.Tensor([[[0, 1, 2, 0, 0], [0, 0, 0, 3, 4]]]).long() # 0 is <pad>, this means that
     # batch=1, num_token=2, num_subword=5, the first token is subword_1 and subword_2, the second token is subword_3 and
     # subword_4
+    # the order can be random. for example, [0,1,2,0,0] is the same as [0,0,2,1,0]
     encoded_layers = torch.rand(1, 5, 1)
     output = average_pooling(encoded_layers, token_subword_index)
     print(output)
